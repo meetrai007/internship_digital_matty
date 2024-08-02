73d303ce00f0a24ce31e94d11213630874601099
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-02 23:14:58 +0530

    add some new feature like no. of atempts to complete game

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -3,17 +3,19 @@ import random
 """to pick a random number and store in guess"""
 guess=random.randint(1,100)
 user_guess=None
+etempts=0
 
 print("welcome to the guess the number game\n")
 while guess != user_guess :
+    etempts +=1
     try:
         user_guess=int(input("guess a number between 1 to 100:"))
     except Exception as ex:
         print("Enter a valid integer number between 1-100")
         continue
     if user_guess==guess:
-        print("congratulation, you guess the right number")
-    elif (guess - 5) <= user_guess <= (guess + 5):
+        print(f"congratulation, you guess the right number with {etempts} etempts")
+    elif (guess - 2) <= user_guess <= (guess + 2):
         print("you are so close")
     elif user_guess<guess:
         print("your guess is to low")
