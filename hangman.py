f7fbcb020e02386acdca9220c124d9d1080b9aba
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-05 23:16:23 +0530

    fix buges

--- hangman.py
+++ hangman.py
@@ -1,29 +1,34 @@
 import random
 
-world_list=["hello","hii","computer"]
-guess=random.choice(world_list)
+word_list=["hello","hii","computer"]
+guess=random.choice(word_list)
 chance=5
+word="-" * (len(guess))
 
-for i in guess:
-    print("-",end="")
+print(f"guess the word:{word}")
 
 while True:
-    
-    user_guess=str(input("\nEnter your guess alphabet"))
 
-    for k in range (len(guess)):
-        if user_guess==guess[k]:
-            newworld=world[:k]+ user_guess +world[k:]
-            
-        
-    print(newworld)
-            
-    chance-=1
-    if chance==0:
-        print("youare loss the game")
-        break
-    
-    if guess==world:
+    user_guess=str(input("\nEnter your guess alphabet:"))
+
+    if user_guess in guess:
+        new_word=""
+        for i in range (len(guess)):
+            if user_guess==guess[i]:
+                new_word+=user_guess
+            else:
+                new_word+=word[i]
+        word=new_word
+        print(f"current word:{new_word}")
+
+    else:
+        chance-=1
+        print(f"entred wrord,your chanse left:{chance}")
+        if chance==0:
+            print("you are loss the game")
+            break
+
+    if guess==word:
         print("you are win the game")
         break
         
