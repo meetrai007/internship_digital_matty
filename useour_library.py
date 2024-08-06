1bd50c78ba956302e74eb7e5b086116ca1671b7e
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-06 14:04:03 +0530

    merge with dev

--- useour_library.py
+++
@@ -1,5 +0,0 @@
-from weather_fatch import find_temp
-
-loc=str(input("Enter location name to search temperature:"))
-
-find_temp(loc)
--- weather_fatch.py
+++ weather_fatch.py
@@ -4,7 +4,7 @@ import json
 
 
 def find_temp(location):
-    # location=str(input("Enter your location to find weather:"))
+    location=str(input("Enter your location to find weather:"))
     user_key=("720eb36df944497d90661207242807")
     url=(f"http://api.weatherapi.com/v1/current.json?key={user_key}&q={location}&aqi=yes")
     response=requests.get(url,timeout=10)
@@ -15,17 +15,3 @@ def find_temp(location):
     temp=weather_data["current"]["temp_c"]
 
     print(f"temp in {loc} is {temp} celsius")
-
-
-
-# location=str(input("Enter your location to find weather:"))
-# user_key=("720eb36df944497d90661207242807")
-# url=(f"http://api.weatherapi.com/v1/current.json?key={user_key}&q={location}&aqi=yes")
-# response=requests.get(url,timeout=10)
-
-# weather_data=response.json()
-
-# loc=weather_data["location"]["name"]
-# temp=weather_data["current"]["temp_c"]
-
-# print(f"temp in {loc} is {temp} celsius")
